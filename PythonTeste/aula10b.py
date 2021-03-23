fb6f0bc9e39043673c9ae40e693e68dea471146a
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-23 16:35:07 -0300

    Aula 10b

---
+++ PythonTeste/aula10b.py
@@ -0,0 +1,13 @@
+#n1 = float(input('Digite a primeira nota: '))
+#n2 = float(input('Digite a segunda nota: '))
+#m = (n1 + n2) / 2
+#print('A sua média foi {:.1f}.'.format(m))
+#print('Parabéns!' if m >= 6.0 else 'Estude mais!')
+
+
+nota1 = float(input('Qual a sua primeira nota? '))
+nota2 = float(input('Qual a sua segunda nota? '))
+nota3 = float(input('Qual a sua terceira nota? '))
+nota4 = float(input('Qual a sua quarta nota? '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print('Sua média final foi: {} '.format(media))
