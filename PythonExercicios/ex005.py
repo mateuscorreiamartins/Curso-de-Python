f24141ce0798313c6e42bd0e9ebfd7a4e51aef2b
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-24 11:46:42 -0300

    Exercício 005

---
+++ PythonExercicios/ex005.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia um número inteiro e mostre 
+# na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número inteiro: '))
+print('O sucessor desse número é: {}. O antecessor desse número é: {}.'.format(n+1, n-1))
+
+i = int(input('Digite um número inteiro: '))
+s = i+1
+a = i-1
+print('O sucessor desse número é: {}. O antecessor desse número é: {}.'.format(s, a))
