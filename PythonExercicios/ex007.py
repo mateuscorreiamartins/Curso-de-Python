389f23785a0a5e1234b9175f98bbaa6a9cae2dc3
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-24 11:54:10 -0300

    Exercício 007

---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, 
+# calcule e mostre a sua média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('A média das duas notas é: {}'.format((n1+n2)/2))
