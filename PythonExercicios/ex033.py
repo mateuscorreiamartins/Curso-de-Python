cec290dbcd7df01aece18a8dbdc33896dd7ad4b6
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-29 15:15:39 -0300

    Exercício 33 - term

--- PythonExercicios/ex033.py
+++ PythonExercicios/ex033.py
@@ -4,3 +4,15 @@
 n1 = int(input('Qual o número? '))
 n2 = int(input('Qual o número? '))
 n3 = int(input('Qual o número? '))
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('\033[1;31mO menor número é {}.'.format(menor))
+print('\033[1;36mO maior número é {}.'.format(maior))
