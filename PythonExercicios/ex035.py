2a77e334b4f5ed503e0eedf48fb512274c019500
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-29 15:18:41 -0300

    Exercício 35 - term

--- PythonExercicios/ex035.py
+++ PythonExercicios/ex035.py
@@ -4,3 +4,7 @@
 r1 = float(input('Qual o comprimento da primeira reta? '))
 r2 = float(input('Qual o comprimento da segunda reta? '))
 r3 = float(input('Qual o comprimento da terceira reta? '))
+if r3 < r1+ r2 and r2 < r1 + r3 and r1 < r2 + r3:
+    print('\033[1;32mIsso pode ser um triângulo.\033[m ')
+else:
+    print('\033[1;31mIsso não pode ser um triângulo. ')
