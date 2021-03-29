fc9f3f63a00010a4d7bc5744c2c3f86eab6e6a0f
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-29 15:18:02 -0300

    Exercício 35

---
+++ PythonExercicios/ex035.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia o comprimento de três retas e
+# diga ao usuário se elas podem ou não formar um triângulo.
+
+r1 = float(input('Qual o comprimento da primeira reta? '))
+r2 = float(input('Qual o comprimento da segunda reta? '))
+r3 = float(input('Qual o comprimento da terceira reta? '))
