0b553189d8df4b62d248b611819873b34d0e33be
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-29 15:15:03 -0300

    Exercício 33

---
+++ PythonExercicios/ex033.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia três números e mostre qual é o maior
+# e qual é o menor.
+
+n1 = int(input('Qual o número? '))
+n2 = int(input('Qual o número? '))
+n3 = int(input('Qual o número? '))
