5661e7028f69d3667e63e7c5d1f6772b7bbb2dd1
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-30 13:52:09 -0300

    Exercício 36 - term

--- PythonExercicios/ex036.py
+++ PythonExercicios/ex036.py
@@ -6,3 +6,11 @@ print('\033[4;35mOlá, como vai?\033[m')
 v = float(input('\nQual o valor da casa? R$'))
 s = float(input('Qual o seu salário? R$'))
 anos = int(input('Em quantos anos você vai pagar? '))
+mensalidade = v / (anos * 12)
+por_sal = s * 30 / 100
+if mensalidade > por_sal:
+    print('\n\033[1;31mO empréstimo foi negado, pois a mensalidade de {:.2f} é maior que 30% do seu salário.\033[m'.format(mensalidade))
+else:
+    print('\n'
+          '\033[1;36mParabéns, seu empréstimo foi aprovado! '
+          'Você terá que pagar R${:.2f} por {} anos.\033[m'.format(mensalidade, anos))
