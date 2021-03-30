9e7d19084288b4cb59a7723d059c2685dc3555bd
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-30 13:54:09 -0300

    Exercício 37 - term

--- PythonExercicios/ex037.py
+++ PythonExercicios/ex037.py
@@ -5,3 +5,11 @@ n = int(input('Digite um número inteiro: '))
 dec = int(input('''[ 1 ] converter para \033[1;34mBINÁRIO\033[m
 [ 2 ] converter para \033[1;33mOCTAL\033[m
 [ 3 ] converter para \033[1;36mHEXADECIMAL\033[m\n'''))
+if dec == 1:
+    print('O número {} em binário é {}'.format(n, bin(n)[2:]))
+elif dec == 2:
+    print('O número {} em octal é {}'.format(n, oct(n)[2:]))
+elif dec == 3:
+    print('O número {} em hexadecimal é {}'.format(n, hex(n)[2:]))
+else:
+    print('\033[4;31mÉ 1, 2 ou 3, seu cabeçudo!')
