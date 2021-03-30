0e30067e8212129cc84de548fbe1ff5a23d11511
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-30 13:53:39 -0300

    Exercício 37

---
+++ PythonExercicios/ex037.py
@@ -0,0 +1,7 @@
+#  Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual
+# será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+
+n = int(input('Digite um número inteiro: '))
+dec = int(input('''[ 1 ] converter para \033[1;34mBINÁRIO\033[m
+[ 2 ] converter para \033[1;33mOCTAL\033[m
+[ 3 ] converter para \033[1;36mHEXADECIMAL\033[m\n'''))
