3e5fb677cf0e149e96b895ee60959bdd5f0c4212
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-31 15:58:25 -0300

    Exercício 39 - 1

--- PythonExercicios/ex039.py
+++ PythonExercicios/ex039.py
@@ -1,3 +1,10 @@
 # Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
 # se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou
 # do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+atual = date.today().year
+sexo = int(input('\n\033[1;30mOlá! Qual o seu sexo?\n\n'
+                 '\033[1;34m[ 1 ] MASCULINO\n'
+                 '\033[1;36m[ 2 ] FEMININO\n\033[m'
+                 '\n\033[1;30mResposta: \033[m'))
