b09c819afb54e9cc97ed38ad2142624ea7922078
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-31 15:54:37 -0300

    Exercício 38

---
+++ PythonExercicios/ex038.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+# O primeiro valor é maior
+# O segundo valor é maior
+# Não existe valor maior, os dois são iguais.
+
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
