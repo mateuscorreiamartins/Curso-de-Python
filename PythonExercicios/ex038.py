2dc83b1f286077bc9476aedd5c5b6d05ffc68d0a
mateuscorreiamartins <mateus46885@gmail.com>
2021-03-31 15:55:12 -0300

    Exercício 38 - term

--- PythonExercicios/ex038.py
+++ PythonExercicios/ex038.py
@@ -5,3 +5,9 @@
 
 n1 = int(input('Digite um número inteiro: '))
 n2 = int(input('Digite outro número inteiro: '))
+if n1 > n2:
+    print('\033[1;35mO primeiro número é maior. ')
+elif n2 > n1:
+    print('\033[1;36mO segundo número é maior. ')
+elif n1 == n2:
+    print('\033[1;33mNão existe número maior, pois os dois são iguais. ')
