5401b56dd4939ec8c37a05197c33362949d9eecd
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-01 21:42:08 -0300

    Exercício 43

--- PythonExercicios/ex043.py
+++ PythonExercicios/ex043.py
@@ -5,3 +5,8 @@
 # 25 até 30: Sobrepeso
 # 30 até 40: Obesidade
 # Acima de 40: Obesidade Mórbida
+
+altura = float(input('Qual é a sua altura em metros? '))
+peso = float(input('Qual é o seu peso em Kgs? '))
+imc = peso / (altura ** 2)
+print('\033[1;30mSeu IMC é \033[m{:.2f}'.format(imc))
