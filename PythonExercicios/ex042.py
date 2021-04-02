00fe360479b140b0c6cb36b189aa1b9ef0794044
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-01 21:38:18 -0300

    Exercício 42

---
+++ PythonExercicios/ex042.py
@@ -0,0 +1,8 @@
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+# EQUILÁTERO: todos os lados iguais
+# ISÓSCELES: dois lados iguais, um diferente
+# ESCALENO: todos os lados diferentes
+
+r1 = int(input('\nQual a medida da primeira reta? '))
+r2 = int(input('Qual a medida da segunda reta? '))
+r3 = int(input('Qual a medida da terceira reta? '))
