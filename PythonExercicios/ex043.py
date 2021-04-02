e1ae24f83514ec444292fb6aba62a1c8684e7ae2
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-01 21:42:42 -0300

    Exercício 43 +

--- PythonExercicios/ex043.py
+++ PythonExercicios/ex043.py
@@ -10,3 +10,9 @@ altura = float(input('Qual é a sua altura em metros? '))
 peso = float(input('Qual é o seu peso em Kgs? '))
 imc = peso / (altura ** 2)
 print('\033[1;30mSeu IMC é \033[m{:.2f}'.format(imc))
+if imc < 18.5:
+    print('\033[4;31mVocê está abaixo do peso!\033[m ')
+elif imc >= 18.5 and imc < 25:
+    print('\033[4;36mVocê está no peso ideal! Parabéns!\033[m')
+elif imc >= 25 and imc < 30:
+    print('\033[1;32mVocê está com sobrepeso!\033[m')
