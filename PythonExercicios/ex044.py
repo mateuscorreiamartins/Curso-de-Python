961578b130231413da0bcb9a630b34359b2218e1
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-03 20:55:19 -0300

    Exercício 44 +

--- PythonExercicios/ex044.py
+++ PythonExercicios/ex044.py
@@ -12,3 +12,9 @@ cond = int(input('''\033[1;34m[ 1 ] À VISTA DINHEIRO/CHEQUE: 10% DE DESCONTO.\0
 \033[1;33m[ 2 ] À VISTA NO CARTÃO: 5% DE DESCONTO.\033[m
 \033[1;35m[ 3 ] EM ATÉ 2x NO CARTÃO: PREÇO NORMAL.\033[m
 \033[1;32m[ 4 ] 3x OU MAIS NO CARTÃO: 20% DE JUROS.\033[m\n'''))
+if cond == 1:
+    print('\n\033[4;30mO valor de R${} com desconto de 10% fica R${}\033[m'.format(v, v - (10 * v / 100)))
+elif cond == 2:
+    print('\n\033[4;30mO valor de R${} com desconto de 5% fica R${}\033[m'.format(v, v - (5 * v / 100)))
+elif cond == 3:
+    print('\n\033[4;30mO valor de R${} deverá ser pago em duas vezes de R${}'.format(v, v / 2))
