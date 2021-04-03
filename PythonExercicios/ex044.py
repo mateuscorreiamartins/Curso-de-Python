7007a9f2d885935f325ae28f9cc8ccb730b70ab1
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-03 20:54:28 -0300

    Exercício 44

--- PythonExercicios/ex044.py
+++ PythonExercicios/ex044.py
@@ -5,3 +5,10 @@
 # à vista no cartão: 5% de desconto
 # em até 2x no cartão: preço normal
 # 3x ou mais no cartão: 20% de juros
+
+print('{:=^40}'.format(' LOJAS TEUZIN '))
+v = float(input('Qual é o valor do produto? R$'))
+cond = int(input('''\033[1;34m[ 1 ] À VISTA DINHEIRO/CHEQUE: 10% DE DESCONTO.\033[m
+\033[1;33m[ 2 ] À VISTA NO CARTÃO: 5% DE DESCONTO.\033[m
+\033[1;35m[ 3 ] EM ATÉ 2x NO CARTÃO: PREÇO NORMAL.\033[m
+\033[1;32m[ 4 ] 3x OU MAIS NO CARTÃO: 20% DE JUROS.\033[m\n'''))
