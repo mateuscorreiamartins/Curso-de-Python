33e63a9d1dfb660a3ffe1f80e5868ae250b76b52
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-04 20:53:28 -0300

    Exercício 45b

---
+++ PythonExercicios/ex045b.py
@@ -0,0 +1,10 @@
+# O QUE O GUANABARA FEZ
+
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
