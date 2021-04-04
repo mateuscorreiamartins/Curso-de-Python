3f1d14a449d41626a220f4fdfece4c4eda8649b2
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-04 20:54:08 -0300

    Exercício 45b +

--- PythonExercicios/ex045b.py
+++ PythonExercicios/ex045b.py
@@ -8,3 +8,10 @@ print('''Suas opções:
 [ 0 ] Pedra
 [ 1 ] Papel
 [ 2 ] Tesoura''')
+jogador = int(input('\nQual a sua jogada? '))
+print('\nJO')
+sleep(1.3)
+print('KEN')
+sleep(1.3)
+print('PÔ\n')
+print('-=' * 14)
