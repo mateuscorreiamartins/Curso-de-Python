70bca868fe444d2fdb6f5562a5ba088559bf5618
mateuscorreiamartins <mateus46885@gmail.com>
2021-04-05 17:47:54 -0300

    Exercício 46

--- PythonExercicios/ex046.py
+++ PythonExercicios/ex046.py
@@ -1,2 +1,5 @@
 #  Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
 # indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+print('\n\033[1;30mVAI COMEÇAR A CONTAGEM REGRESSIVA!\033[m')
